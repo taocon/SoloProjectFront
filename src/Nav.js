ca9ea3872a0b85f34f35939349a4e12536ac7005
taocon <conradtta@gmail.com>
2019-02-08 13:51:58 +0000

    cleanup

--- src/Nav.js
+++ src/Nav.js
@@ -6,7 +6,6 @@ import {
     Link
 } from 'react-router-dom'
 import Listofitems from "./Listofitems.js";
-import Calculator from "./Calculator.js";
 import Accounts from "./Accounts.js";
 import Homepage from "./HomePage.js";
 import ItemComponent from "./ItemComponent.js";
@@ -24,8 +23,7 @@ class Nav extends Component {
             <ul id = "navigationbarul">
             <li id = "i"><Link to="/">Home</Link></li>
             <li id = "i"><Link to="/items">ItemDatabase</Link></li>
-            <li id = "i"><Link to="/calc">Calculator</Link></li>
-            <li id = "i"><Link to="/acc">Accounts</Link></li>
+             <li id = "i"><Link to="/acc">Accounts</Link></li>
             <li id = "i"><Link to="/login">Login</Link></li>
             </ul>
            
@@ -34,7 +32,6 @@ class Nav extends Component {
 
             <Route exact path="/" component={Homepage} />
             <Route path="/items" component={Listofitems} />
-            <Route path="/calc" component={Calculator} />
             <Route path="/acc" component={Accounts} />
             <Route path="/item/:id" component={ItemComponent} />
             <Route path="/login" exact component={Login} />
